708db88b9c1b490f630d6e8cf22d4f643721e3ce
ManuelaBarth <manubarth.jippie8390@gmail.com>
2024-02-28 16:37:50 +0100

    added validation for at least 2 players incl. exception

--- test/java/com/uglytrivia/GameTest.java
+++ test/java/com/uglytrivia/GameTest.java
@@ -1,8 +1,11 @@
 package com.uglytrivia;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
+import java.util.Random;
 import main.java.com.uglytrivia.Game;
+import main.java.com.uglytrivia.GameRunner;
 import main.java.com.uglytrivia.Player;
 import org.junit.Test;
 
@@ -35,5 +38,25 @@ public class GameTest {
     aGame.add(name2);
 
     assertEquals(expectedPlayerTwo.toString(), aGame.getNextPlayer().toString());
+  }
+
+  @Test
+  public void shouldThrowExceptionWithLessThanTwoPlayers() {
+    Game aGame = new Game();
+    String name1 = "Manu";
+    Player playerOne = new Player(name1);
+    playerOne.setCurrentPlayer(true);
+    aGame.add(name1);
+
+    Exception exception =
+        assertThrows(
+            InstantiationException.class,
+            () -> {
+              GameRunner.playGame(new Random(), aGame);
+            });
+
+    String expectedMessage = "A Game needs at least two players.";
+    String actualMessage = exception.getMessage();
+    assertEquals(expectedMessage, actualMessage);
   }
 }
