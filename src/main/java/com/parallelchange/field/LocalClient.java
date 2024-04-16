f47ee9a089fa720ef134f7b27169cbd2acda2954
ManuelaBarth <manubarth.jippie8390@gmail.com>
2024-04-16 15:15:27 +0200

    switched local client to list parameter

--- src/main/java/com/parallelchange/field/LocalClient.java
+++ src/main/java/com/parallelchange/field/LocalClient.java
@@ -1,10 +1,12 @@
 package main.java.com.parallelchange.field;
 
+import java.util.Arrays;
+
 public class LocalClient {
 
     public static void main(String[] args) {
         ShoppingCart shoppingCart = new ShoppingCart();
-        shoppingCart.add(10);
+        shoppingCart.add(Arrays.asList(10));
         System.out.println("shoppingCart.numberOfProducts() = " + shoppingCart.numberOfProducts());
         System.out.println("shoppingCart.calculateTotalPrice() = " + shoppingCart.calculateTotalPrice());
         System.out.println("shoppingCart.hasDiscount() = " + shoppingCart.hasDiscount());
