de754e1d90ea2bfbab8e46610a0266831fce4627
ManuelaBarth <manubarth.jippie8390@gmail.com>
2024-04-16 15:14:03 +0200

    client removes use of method with single price

--- src/main/java/com/parallelchange/field/ImagineThisIsAClientInADifferentRepository.java
+++ src/main/java/com/parallelchange/field/ImagineThisIsAClientInADifferentRepository.java
@@ -3,11 +3,6 @@ package main.java.com.parallelchange.field;
 import java.util.List;
 
 public class ImagineThisIsAClientInADifferentRepository {
-    public String formattedTotalPrice(int price) {
-        ShoppingCart shoppingCart = new ShoppingCart();
-        shoppingCart.add(price);
-        return String.format("Total price is %d euro", shoppingCart.calculateTotalPrice());
-    }
 
     public String formattedTotalPrice(List<Integer> priceList) {
         ShoppingCart shoppingCart = new ShoppingCart();
