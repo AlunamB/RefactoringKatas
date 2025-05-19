40cf940c26a855986b87a300e6b56329fa0e3577
AlunamB <96864660+AlunamB@users.noreply.github.com>
2025-05-19 10:50:16 +0200

    first test implemented that checks wheather a value is returned if a diced was rolled

--- src/main/java/com/greedgame/GreedGame.java
+++ src/main/java/com/greedgame/GreedGame.java
@@ -2,6 +2,12 @@ package main.java.com.greedgame;
 
 public class GreedGame {
 
+    private int diceRoll = -1;
+
+    public GreedGame(int diceRoll){
+        this.diceRoll = diceRoll;
+    }
+
 
     public int score(){
         return 0;
--- test/java/com/greedgame/StepDefinitions.java
+++ test/java/com/greedgame/StepDefinitions.java
@@ -7,19 +7,25 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import main.java.com.gildedrose.GildedRose;
 import main.java.com.gildedrose.Item;
+import main.java.com.greedgame.GreedGame;
 
 public class StepDefinitions {
 
+    private int score;
+    private GreedGame greedGame;
+
     @Given("The dice shows {int}")
     public void theDiceShows(int arg0) {
+        greedGame = new GreedGame(arg0);
     }
 
     @When("I calculate the score")
     public void iCalculateTheScore() {
-        
+        score = greedGame.score();
     }
 
     @Then("I should get a score of {int}")
-    public void iShouldGetAScoreOf(int arg0) {
+    public void iShouldGetAScoreOf(int expectedScore) {
+        assertEquals(score, expectedScore);
     }
 }
